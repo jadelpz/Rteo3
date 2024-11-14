bd5b1ebe1e2a68d7e5fb232b8fb269c006d1a243
DAM1 <jade>
2024-11-14 08:42:35 +0100

    Dossss

--- Rteto3/src/Reto3/Reto3.java
+++ Rteto3/src/Reto3/Reto3.java
@@ -1,10 +1,27 @@
 package Reto3;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+import util.Funciones;
+
 public class Reto3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		/*Crea un programa que pida dos fechas por consola, validando que la 
+		 segunda sea mayor en el tiempo que la
+		primera. Si no lo es, vuelve a pedir la segunda fecha hasta que lo sea.
+		Con esas fechas:*/
+		Scanner sc = new Scanner (System.in);
+		Funciones.dimeFecha("dime fecha: ", sc);
+	
 	}
-
+	
+	
+	
+	
+	
+	
 }
---
+++ Rteto3/src/util/Funciones.java
@@ -0,0 +1,65 @@
+package util;
+
+import java.util.Scanner;
+
+public class Funciones {
+	//indica si el String que recibe como parÃ¡metro es entero
+	public static boolean esInt(String s)
+	{
+		try {
+			Integer.parseInt(s);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	
+	//indica si el String que recibe como parÃ¡metro es decimal
+	public static boolean esDouble(String s)
+	{
+		try {
+			Double.parseDouble(s);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	
+	//pide algo por consola hasta que sea un nÃºmero entero, el mensaje que pide ese algo se pasa
+	//por parÃ¡metro para poder personalizarlo
+	public static int dimeEntero(String orden, Scanner sc)
+	{
+		do
+		{
+		try {
+			System.out.println(orden);
+			String s = sc.nextLine();
+			int n= Integer.parseInt(s);
+			return n;
+
+		} catch (Exception e) {
+			System.out.println("Formato incorrecto");
+		}
+		}while(true);
+	}
+	
+	//pide algo por consola hasta que sea un nÃºmero decimal, el mensaje que pide ese algo se pasa
+	//por parÃ¡metro para poder personalizarlo
+	public static double dimeDouble(String orden, Scanner sc)
+	{
+		do
+		{
+		try {
+			System.out.println(orden);
+			String s = sc.nextLine();
+			double d= Double.parseDouble(s);
+			return d;
+
+		} catch (Exception e) {
+			System.out.println("Formato incorrecto");
+		}
+		}while(true);
+	}
+	
+	
+}
